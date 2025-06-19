19ecb0b061494bb13c9e4502f51264783c82968a
eremean89 <eremean89@gmail.com>
2025-06-19 21:05:33 +0300

    images

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,17 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig: import('next').NextConfig = {
+const nextConfig: import("next").NextConfig = {
+  images: {
+    // отключаем оптимизацию для всех изображений
+    unoptimized: true,
+  },
   webpack(config) {
     config.module.rules.unshift({
       test: /\.html$/,
-      loader: 'ignore-loader'
+      loader: "ignore-loader",
     });
     return config;
-  }
+  },
 };
 
 export default nextConfig;
