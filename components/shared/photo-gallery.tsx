f4aa67ddde262cd404c9945e3596d3043a2405d7
eremean89 <eremean89@gmail.com>
2025-06-21 19:51:37 +0300

    img

--- components/shared/photo-gallery.tsx
+++ components/shared/photo-gallery.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { useState, useRef, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { ChevronLeft, ChevronRight } from "lucide-react";
